0632619ea0e3aea2eae0cccfa6a2c6c16831c809
Ifpk <bernardofroes10@gmail.com>
2024-06-22 12:07:38 -0400

    first commit

---
+++ exemplo1.py
@@ -0,0 +1,17 @@
+#str - strings - texto
+#int - inteiros
+#float - numeros de ponto flutuante
+#bool - booleanos - True False
+#NoType - sem tipo
+print("2+2")
+print(2+2)
+print(2-2)
+print(2*2)
+print(3/2)
+print(3//2)
+print(1535/7)
+print(1535//7)
+
+print(2**2)
+
+print(5%2)
